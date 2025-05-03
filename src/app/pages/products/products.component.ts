456d4f6e3f65862895f82f08e060a8dfdca2667a
pritam <pritamsgvu@gmail.com>
2025-05-03 18:49:59 +0530

    menu pdf

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  ViewChild, ElementRef  } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ProductService } from '../../services/product.service';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-product-management',
@@ -11,8 +13,12 @@ export class ProductsComponent implements OnInit {
   productForm: FormGroup;
   products: any[] = [];
   services: any[] = [];
+  groupedServices: any[] = [];
+
   selectedProduct: any = null;
   loading: boolean = true;
+  @ViewChild('menuContent') menuContent!: ElementRef;
+
 
   constructor(private fb: FormBuilder, private productService: ProductService) {
     this.productForm = this.fb.group({
@@ -34,8 +40,26 @@ export class ProductsComponent implements OnInit {
   loadProducts() {
     this.productService.getProducts().subscribe(data => {
       this.products = data;
+      this.groupProductsByService();
       this.loading = false;
     });
+  }
+  groupProductsByService() {
+    const map: any = {};
+  
+    this.products.forEach(product => {
+      product.services.forEach((service: any) => {
+        if (!map[service.serviceName]) {
+          map[service.serviceName] = [];
+        }
+        map[service.serviceName].push(product);
+      });
+    });
+  
+    this.groupedServices = Object.keys(map).map(serviceName => ({
+      serviceName,
+      products: map[serviceName]
+    }));
   }
 
   loadServices() {
@@ -93,4 +117,40 @@ export class ProductsComponent implements OnInit {
       services: []
     });
   }
+
+  getProductsForService(serviceId: string) {
+    return this.products.filter(product =>
+      product.services.some((s: any) => s._id === serviceId)
+    );
+  }
+  
+
+  downloadPdf() {
+    const content = this.menuContent.nativeElement;
+
+    html2canvas(content, { scale: 1.5, useCORS: true }).then(canvas => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    
+      // Add the image to the first page
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      
+      let heightLeft = pdfHeight;
+      
+      // Add additional pages if needed
+      while (heightLeft > pdf.internal.pageSize.getHeight()) {
+        heightLeft -= pdf.internal.pageSize.getHeight();
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, -heightLeft, pdfWidth, pdfHeight);
+      }
+    
+      pdf.save('brobarber-menu.pdf');
+    });
+    
+  }
+  
+  
 }
